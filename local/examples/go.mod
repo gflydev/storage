02ba73090c199afabc04724ea1df4645191fe113
Vinh Vo <vohuynhvinh@gmail.com>
2024-12-12 11:17:59 +0700

    Upgrade core

---
+++ local/examples/go.mod
@@ -0,0 +1,23 @@
+module examples
+
+go 1.22.6
+
+replace github.com/gflydev/storage => ../../
+
+replace github.com/gflydev/storage/local => ../
+
+require (
+	github.com/gflydev/core v1.9.0
+	github.com/gflydev/view/pongo v1.0.2
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/flosch/pongo2/v6 v6.0.0 // indirect
+	github.com/gflydev/storage v1.1.0 // indirect
+	github.com/gflydev/storage/local v1.0.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.55.0 // indirect
+)
---
+++ local/examples/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gflydev/core"
+	"github.com/gflydev/core/utils"
+	"github.com/gflydev/storage"
+	_ "github.com/gflydev/storage/local"
+	"github.com/gflydev/view/pongo"
+)
+
+// =========================================================================================
+//                                     Default API
+// =========================================================================================
+
+// NewDefaultApi As a constructor to create new API.
+func NewDefaultApi() *DefaultApi {
+	return &DefaultApi{}
+}
+
+// DefaultApi API struct.
+type DefaultApi struct {
+	core.Api
+}
+
+func (h *DefaultApi) Handle(c *core.Ctx) error {
+	return c.JSON(core.Data{
+		"name":   core.AppName,
+		"server": core.AppURL,
+	})
+}
+
+// =========================================================================================
+//                                     Home page
+// =========================================================================================
+
+// NewHomePage As a constructor to create a Home Page.
+func NewHomePage() *HomePage {
+	return &HomePage{}
+}
+
+type HomePage struct {
+	core.Page
+}
+
+func (m *HomePage) Handle(c *core.Ctx) error {
+	fs := storage.Instance()
+
+	if ok := fs.MakeDir("one/two"); ok {
+		fs.Put("one/two/hello.txt", "Hello world")
+	}
+
+	return c.String("Hello world")
+}
+
+// =========================================================================================
+//                                     Routers
+// =========================================================================================
+
+func router(g core.IFlyRouter) {
+	prefixAPI := fmt.Sprintf(
+		"/%s/%s",
+		utils.Getenv("API_PREFIX", "api"),
+		utils.Getenv("API_VERSION", "v1"),
+	)
+
+	// API Routers
+	g.Group(prefixAPI, func(apiRouter *core.Group) {
+		apiRouter.GET("/info", NewDefaultApi())
+	})
+
+	// Web Routers
+	g.GET("/home", NewHomePage())
+}
+
+// =========================================================================================
+//                                     Application
+// =========================================================================================
+
+func main() {
+	app := core.New()
+
+	// Register view
+	core.RegisterView(pongo.New())
+
+	// Register router
+	app.RegisterRouter(router)
+
+	app.Run()
+}
